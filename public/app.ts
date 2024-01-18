c17633b2cbb3812c9458fa6914e7e71ed6a4be9b
Assaf Basri <Faskoman90@gmail.com>
2024-01-18 23:19:20 +0200

    added logout func and button to header

--- public/app.ts
+++ public/app.ts
@@ -1,10 +1,11 @@
-import { getJSON, handleUser, redirect } from "./funcs.js";
+import { getJSON, handleUser, logout, redirect } from "./funcs.js";
 
 async function app() {
   const user = await getJSON("/api/auth/currentUser");
-  
+
   handleUser(user);
   redirect();
+  logout();
 }
 
 app();
