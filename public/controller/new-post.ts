15e03a2906d1f8318540f3d5afe05039fc02e0c3
Assaf Basri <Faskoman90@gmail.com>
2024-01-20 20:30:44 +0200

    new-post form

--- public/controller/new-post.ts
+++ public/controller/new-post.ts
@@ -2,8 +2,10 @@ import {
   clickSound,
   getCurrentUser,
   handleUser,
+  hideDisplay,
   logout,
   redirect,
+  showDisplay,
 } from "./funcs.js";
 
 async function app() {
@@ -12,6 +14,61 @@ async function app() {
   handleUser(user);
   logout();
   redirect();
+  togglePayment();
+
+  document.forms.namedItem("newPost")?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.target as HTMLFormElement);
+      const body = JSON.stringify({
+        title: formData.get("title"),
+        type: formData.get("type"),
+        neighborhood: formData.get("neighborhood"),
+        city: formData.get("city"),
+        bedrooms: formData.get("bedrooms"),
+        floor: formData.get("floor"),
+        squareMeters: formData.get("squareMeters"),
+        transaction: formData.get("transaction"),
+        cost: {
+          monthlyRentInNIS: formData.get("monthlyRentInNIS"),
+          priceInNIS: formData.get("priceInNIS"),
+        },
+        amenities: {
+          hasBalcony: formData.get("hasBalcony") === "true",
+          hasParking: formData.get("hasParking") === "true",
+          hasAC: formData.get("hasAC") === "true",
+          arePetsAllowed: formData.get("arePetsAllowed") === "true",
+          isSmokingAllowed: formData.get("isSmokingAllowed") === "true",
+          hasElevator: formData.get("hasElevator") === "true",
+        },
+        description: formData.get("description"),
+        images: formData.get("images"),
+        contactInformation: {
+          name: user.username,
+          phone: user.phone,
+          email: user.email,
+        },
+        availabilityDate: formData.get("availabilityDate") || null,
+      });
+
+      const res = await fetch("/view/api/properties/new-post", {
+        method: "POST",
+        body,
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": body.length.toString(),
+        },
+      });
+
+      if (res.status >= 400) {
+        throw new Error();
+      }
+      window.location.replace("/");
+    } catch (error) {
+      console.error(error);
+    }
+  });
 }
 
 app();
@@ -23,3 +80,21 @@ labels.forEach((label) => {
     clickSound();
   });
 });
+
+function togglePayment() {
+  document.getElementById("Purchase")?.addEventListener("click", () => {
+    showDisplay(document.getElementById("sell-price") as HTMLDivElement);
+    hideDisplay(document.getElementById("rent-price") as HTMLDivElement);
+    resetInputValue("monthlyRentInNIS");
+  });
+
+  document.getElementById("Lease")?.addEventListener("click", () => {
+    showDisplay(document.getElementById("rent-price") as HTMLDivElement);
+    hideDisplay(document.getElementById("sell-price") as HTMLDivElement);
+    resetInputValue("priceInNIS");
+  });
+}
+
+function resetInputValue(input: string) {
+  (document.getElementById(input) as HTMLInputElement).value = "";
+}
