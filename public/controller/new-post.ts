5ad076b1d768defcab9fb0e320a568059cf139d8
Assaf Basri <Faskoman90@gmail.com>
2024-01-20 22:23:50 +0200

    all checked except amenities, description, images

--- public/controller/new-post.ts
+++ public/controller/new-post.ts
@@ -16,11 +16,29 @@ async function app() {
   redirect();
   togglePayment();
 
+  const phone = user.phone;
+  const name = user.username;
+  const email = user.email;
+
+  sessionStorage.clear();
+
   document.forms.namedItem("newPost")?.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     try {
       const formData = new FormData(e.target as HTMLFormElement);
+      const priceInNIS = formData.get("priceInNIS");
+      const monthlyRentInNIS = formData.get("monthlyRentInNIS");
+      if (!priceInNIS && !monthlyRentInNIS) {
+        throw new Error(
+          "Either priceInNIS or monthlyRentInNIS must be provided."
+        );
+      } else if (
+        (priceInNIS && Number(priceInNIS) <= 0) ||
+        (monthlyRentInNIS && Number(monthlyRentInNIS) <= 0)
+      ) {
+        throw new Error("Cannot be a negative number or 0");
+      }
       const body = JSON.stringify({
         title: formData.get("title"),
         type: formData.get("type"),
@@ -29,7 +47,7 @@ async function app() {
         bedrooms: formData.get("bedrooms"),
         floor: formData.get("floor"),
         squareMeters: formData.get("squareMeters"),
-        transaction: formData.get("transaction"),
+        transaction: sessionStorage.getItem("transaction"),
         cost: {
           monthlyRentInNIS: formData.get("monthlyRentInNIS"),
           priceInNIS: formData.get("priceInNIS"),
@@ -45,9 +63,9 @@ async function app() {
         description: formData.get("description"),
         images: formData.get("images"),
         contactInformation: {
-          name: user.username,
-          phone: user.phone,
-          email: user.email,
+          name: name,
+          phone: phone,
+          email: email,
         },
         availabilityDate: formData.get("availabilityDate") || null,
       });
@@ -64,6 +82,8 @@ async function app() {
       if (res.status >= 400) {
         throw new Error();
       }
+
+      sessionStorage.clear();
       window.location.replace("/");
     } catch (error) {
       console.error(error);
@@ -86,12 +106,14 @@ function togglePayment() {
     showDisplay(document.getElementById("sell-price") as HTMLDivElement);
     hideDisplay(document.getElementById("rent-price") as HTMLDivElement);
     resetInputValue("monthlyRentInNIS");
+    sessionStorage.setItem("transaction", "Purchase");
   });
 
   document.getElementById("Lease")?.addEventListener("click", () => {
     showDisplay(document.getElementById("rent-price") as HTMLDivElement);
     hideDisplay(document.getElementById("sell-price") as HTMLDivElement);
     resetInputValue("priceInNIS");
+    sessionStorage.setItem("transaction", "Lease");
   });
 }
 
